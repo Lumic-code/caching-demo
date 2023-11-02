6a630f8fa412e84178ad5e73b366fefeaab3c376
Lumic-code <wisdomlumic0368@gmail.com>
2023-11-02 22:26:47 +0100

    first part of the project done

---
+++ CachingDemo.Infrastruture/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CachingDemo.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CachingDemo.Infrastruture.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
